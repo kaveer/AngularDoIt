d69feca5717228938b69d10e3b51cbc6ffe5f5ed
kaveer <kaveer.rajcoomar@sdworx.com>
2024-05-15 21:42:36 +0400

    implement method addTodo click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'DoIt';
+  toDoValue: string = ``;
+  toDoList: string[] = [];
+
+  addToDo() {
+    if (this.toDoValue) {
+      this.toDoList.push(this.toDoValue)
+      console.log("Array of strings:", this.toDoList);
+    }
+  }
 }
